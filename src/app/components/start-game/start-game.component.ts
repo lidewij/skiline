1161b9c8bf9daad496365f91c97585fb578acba9
lidewij <lidewij122@gmail.com>
2020-05-15 19:17:48 +0200

    changed socket url

--- src/app/components/start-game/start-game.component.ts
+++ src/app/components/start-game/start-game.component.ts
@@ -14,7 +14,11 @@ export class StartGameComponent implements OnInit {
   }
 
   startWebsocket() {
-    this.socket = new SocketService('http://localhost', 45678);
+     //this.socket = new SocketService('http://localhost', 45678);
+    // this.socket = new SocketService('ws://nodejs-ski-server.herokuapp.com/socket.io/?EIO=4&transport=websocket', 52300);
+    this.socket = new SocketService('ws://nodejs-ski-server.herokuapp.com?EIO=4&transport=websocket', 52300);
+
+
     this.socket.on('connect', () => {
       console.log('connected');
       this.socket.emit('register', 'app');
@@ -34,4 +38,43 @@ export class StartGameComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  // startWebsocket() {
+  //   this.socket = new SocketService('ws://nodejs-ski-server.herokuapp.com:80/socket.io/?EIO=4&transport=websocket', 52300);
+  //   this.socket.connect();
+  //   // this.socket = new SocketService('http://localhost', 45678);
+  //   // this.socket.on('connect', () => {
+  //   //   console.log('connected');
+  //   //   // this.socket.emit('registerApp', 'app');
+  //   // });
+  //   // this.socket.fromEvent('event').subscribe((message) => {
+  //   //   console.log('Message received: ' + message);
+  //   // });
+  //   this.socket.on('registerApp', (data) => {
+  //     console.log('test');
+  //   });
+  //   // this.socket.emit('registeredApp');
+  //
+  //   this.socket.fromEvent('registerApp').subscribe((message) => {
+  //     console.log('test' + message);
+  //   });
+  // }
+  // sendHello() {
+  //   this.socket.emit('registeredApp');
+  // }
+  //
+  // stopWebsocket() {
+  //   this.socket.disconnect();
+  // }
+
+//   ngOnInit() {
+//   //   this.socket = new SocketService('ws://nodejs-ski-server.herokuapp.com:80/socket.io/?EIO=4&transport=websocket', 52300);
+//   //   this.socket.connect();
+//   //
+//   //   this.socket.fromEvent('registerApp').subscribe( (message) => {
+//   //     console.log('test' + message);
+//   //   });
+//   //   this.socket.emit('registeredApp', 'message' );
+//   // }
+// }
 }
